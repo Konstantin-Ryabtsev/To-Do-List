3d583af873a9324850cf58675b0ba7ce94d98f19
Konstantin Ryabtsev <kryabtsev83@gmail.com>
2022-02-18 01:34:32 +0300

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Konstantin Ryabtsev on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
